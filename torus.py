2d045fc937acba6f30a45ba0940c78f6460f81f0
Storsorken <erikwac@kth.se>
2023-06-30 21:08:30 +0200

    torus.py able to input questions from defined page

--- torus.py
+++ torus.py
@@ -5,22 +5,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from env import USER_EMAIL, USER_PASSWORD, CURRICULUM_URL
 from time import sleep
-from question import Question
+from page_definition import Page_definition
 
 
-example_question = """When is it more advantageous to use pointer receivers over value receivers in Go?
-
-    A) When the method needs to modify the receiver value or when the receiver is a large struct.
-    - Correct! Using pointer receivers can be more efficient when the receiver is a large struct because it avoids copying the value. Also, if the method needs to modify the receiver value, a pointer receiver is required.
-
-    B) It is always better to use value receivers in Go.
-    - Incorrect! This is a misconception. While value receivers can be easier to understand and use, there are situations where pointer receivers are more efficient or necessary, such as when the method needs to modify the receiver value or when the receiver is a large struct.
-
-    C) It is always better to use pointer receivers in Go.
-    - Incorrect! This is a misconception. The choice between value receivers and pointer receivers depends on the specific requirements and constraints of the situation. For example, value receivers are typically used when the method does not need to modify the receiver and the receiver is a small struct.
-"""
-question = Question.from_string(example_question)
-
+page_definition = Page_definition.from_file('input.txt')
 
 driver = webdriver.Chrome()
 driver.get(CURRICULUM_URL)
@@ -44,8 +32,7 @@ submit_btn.click()
 sleep(1)
 
 # open unit
-
-driver.find_element(By.PARTIAL_LINK_TEXT, "Unit 2:").click()
+driver.find_element(By.PARTIAL_LINK_TEXT, page_definition.unit).click()
 
 sleep(1)
 
@@ -60,81 +47,76 @@ edit_btn.click()
 # add multiple choice question
 sleep(5)
 
-add_element_menu_btns = driver.find_elements(By.CSS_SELECTOR, ".addResourceContent_W\\+36phqP")
-add_element_menu_btns[-1].click()
+for question in page_definition.questions[:3]:
 
-sleep(1)
+    add_element_menu_btns = driver.find_elements(By.CSS_SELECTOR, ".addResourceContent_W\\+36phqP")
+    add_element_menu_btns[-1].click()
 
-options_bounding_box = wait.until(
-    EC.visibility_of_element_located((By.CLASS_NAME, 'activities'))
-)
-add_MCQ_btn = options_bounding_box.find_elements(By.CLASS_NAME, 'resource-choice')[1]
-add_MCQ_btn.click()
+    sleep(1)
 
-# fill in question
+    options_bounding_box = wait.until(
+        EC.visibility_of_element_located((By.CLASS_NAME, 'activities'))
+    )
+    add_MCQ_btn = options_bounding_box.find_elements(By.CLASS_NAME, 'resource-choice')[1]
+    add_MCQ_btn.click()
 
-sleep(1)
+    # fill in questions
 
-question_block = driver.find_elements(By.CSS_SELECTOR, ".resource-block-editor")[-1]
+    sleep(1)
 
-# add third answer option
-question_block.find_element(By.CSS_SELECTOR, ".addChoiceContainer_nMRQoZI6").find_element(By.CSS_SELECTOR, "button").click()
+    question_block = driver.find_elements(By.CSS_SELECTOR, ".resource-block-editor")[-1]
 
-slate_editors = question_block.find_elements(By.CLASS_NAME, "slate-editor")
-question_input = slate_editors[0]
-alt1_input = slate_editors[1]
-alt2_input = slate_editors[2]
-alt3_input = slate_editors[3]
+    # add third answer option
+    question_block.find_element(By.CSS_SELECTOR, ".addChoiceContainer_nMRQoZI6").find_element(By.CSS_SELECTOR, "button").click()
 
-question_input.send_keys(question.question_text)
-alt1_input.send_keys(Keys.BACKSPACE * 8 + question.answer_options[0])
-alt2_input.send_keys(Keys.BACKSPACE * 8 + question.answer_options[1])
-alt3_input.send_keys(question.answer_options[2])
+    slate_editors = question_block.find_elements(By.CLASS_NAME, "slate-editor")
+    question_input = slate_editors[0]
+    alt1_input = slate_editors[1]
+    alt2_input = slate_editors[2]
+    alt3_input = slate_editors[3]
 
-sleep(1)
+    question_input.send_keys(question.question_text)
+    alt1_input.send_keys(Keys.BACKSPACE * 8 + question.answer_options[0])
+    alt2_input.send_keys(Keys.BACKSPACE * 8 + question.answer_options[1])
+    alt3_input.send_keys(question.answer_options[2])
 
-# add answer feedback
+    sleep(1)
 
-question_block.find_element(By.LINK_TEXT, "ANSWER KEY").click()
+    # add answer feedback
 
-sleep(1)
+    question_block.find_element(By.LINK_TEXT, "ANSWER KEY").click()
 
-# click on add targeted feedback twice
+    sleep(1)
 
-question_block.find_elements(By.CSS_SELECTOR, ".btn.btn-link.pl-0")[-1].click()
-question_block.find_elements(By.CSS_SELECTOR, ".btn.btn-link.pl-0")[-1].click()
+    # click on add targeted feedback twice
 
-# select correct answer
-driver.execute_script("arguments[0].scrollIntoView();", question_block)
-correct_ans_radio_btns = question_block.find_elements(By.CSS_SELECTOR, ".oli-radio.flex-shrink-0")[3:6] # first three are on the question page
-correct_ans_radio_btns[question.correct_option].click()
+    question_block.find_elements(By.CSS_SELECTOR, ".btn.btn-link.pl-0")[-1].click()
+    question_block.find_elements(By.CSS_SELECTOR, ".btn.btn-link.pl-0")[-1].click()
 
-sleep(1)
+    # select correct answer
+    driver.execute_script("arguments[0].scrollIntoView();", question_block)
+    correct_ans_radio_btns = question_block.find_elements(By.CSS_SELECTOR, ".oli-radio.flex-shrink-0")[3:6] # first three are on the question page
+    correct_ans_radio_btns[question.correct_option].click()
 
-div_cards = question_block.find_elements(By.CLASS_NAME, "card")
-# ignore div_cards[1] (wrong answer feedback) because it's not visible when using the targeted feedback
-correct_ans_feedback_div = div_cards[0]
-targeted_feedback_1_div = div_cards[2]
-targeted_feedback_2_div = div_cards[3]
+    sleep(1)
 
-correct_ans_feedback_div.find_element(By.CLASS_NAME, "slate-editor").send_keys(
-    Keys.BACKSPACE * 8 + question.feedback[question.correct_option]
-)
+    div_cards = question_block.find_elements(By.CLASS_NAME, "card")
+    # ignore div_cards[1] (wrong answer feedback) because it's not visible when using the targeted feedback
+    correct_ans_feedback_div = div_cards[0]
+    targeted_feedback_1_div = div_cards[2]
+    targeted_feedback_2_div = div_cards[3]
 
-non_correct_options = [i for i in range(3) if i != question.correct_option]
-select_corresponding_answer_btns = targeted_feedback_1_div.find_elements(By.CSS_SELECTOR, ".oli-radio.flex-shrink-0")
-select_corresponding_answer_btns[non_correct_options[0]].click()
-targeted_feedback_1_div.find_element(By.CLASS_NAME, "slate-editor").send_keys(question.feedback[non_correct_options[0]])
+    correct_ans_feedback_div.find_element(By.CLASS_NAME, "slate-editor").send_keys(
+        Keys.BACKSPACE * 8 + question.feedback[question.correct_option]
+    )
 
-select_corresponding_answer_btns = targeted_feedback_2_div.find_elements(By.CSS_SELECTOR, ".oli-radio.flex-shrink-0")
-select_corresponding_answer_btns[non_correct_options[1]].click()
-targeted_feedback_2_div.find_element(By.CLASS_NAME, "slate-editor").send_keys(question.feedback[non_correct_options[1]])
+    non_correct_options = [i for i in range(3) if i != question.correct_option]
+    select_corresponding_answer_btns = targeted_feedback_1_div.find_elements(By.CSS_SELECTOR, ".oli-radio.flex-shrink-0")
+    select_corresponding_answer_btns[non_correct_options[0]].click()
+    targeted_feedback_1_div.find_element(By.CLASS_NAME, "slate-editor").send_keys(question.feedback[non_correct_options[0]])
+
+    select_corresponding_answer_btns = targeted_feedback_2_div.find_elements(By.CSS_SELECTOR, ".oli-radio.flex-shrink-0")
+    select_corresponding_answer_btns[non_correct_options[1]].click()
+    targeted_feedback_2_div.find_element(By.CLASS_NAME, "slate-editor").send_keys(question.feedback[non_correct_options[1]])
 
 sleep(200)
-
-# new_page = driver.find_element(By.CSS_SELECTOR, "button:contains('Practice Page')")
-# print(new_page.text)
-
-# new_page.click()
-
-
