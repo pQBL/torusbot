84ccb98eded987bb9cf00e641d4e02b901b41146
Storsorken <erikwac@kth.se>
2023-07-11 19:55:38 +0200

    Specify window size
    The hope is that it would lead to better reliability.

--- torusbot.py
+++ torusbot.py
@@ -155,6 +155,7 @@ def main():
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+    chrome_options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=chrome_options)
 
     driver.get(os.getenv('CURRICULUM_URL'))
