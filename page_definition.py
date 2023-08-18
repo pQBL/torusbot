fa28076415c9ba984949759fcfc909f27966e76d
Örn Segerstedt <ornse@kth.se>
2023-08-18 13:42:49 +0200

    Change regex from Unit to Unit_name in page_definition.py

--- page_definition.py
+++ page_definition.py
@@ -12,7 +12,7 @@ class Page_definition:
     @classmethod
     def from_file(cls, page_definition_file: str) -> 'Page_definition':
         with open(page_definition_file) as f:
-            unit = re.search(r'Unit: (.*)', f.readline()).group(1).strip()
+            unit = re.search(r'Unit_name: (.*)', f.readline()).group(1).strip()
             page = re.search(r'Page_name: (.*)', f.readline()).group(1).strip()
             question_blocks = re.split(r'\n\d+\.\s', f.read())
             questions = [Question.from_string(question_block) for question_block in question_blocks[1:]]
